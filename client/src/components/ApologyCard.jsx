fbcf47e9bf1437753f2cda03bb833383dc22b3ae
jangidritesh570-cmyk <jangidritesh570@gmail.com>
2026-06-19 12:32:03 +0530

    apologycard

--- client/src/components/ApologyCard.jsx
+++ client/src/components/ApologyCard.jsx
@@ -9,9 +9,9 @@ function ApologyCard() {
     >
       <h2>🥺 Sorry yrr behan maaf krde please 🥺</h2>
 
-      <p>are yrr kal toh naraj kr diya maine tujhe😂</p>
+      <p>are yrr behan maine faltu hi uss din tujhe naraj kr diya sorry</p>
 
-      <p>ab yrr aise kregi kya tu bhai se naraj hogi naah naah mujhse naraj mt ho chomu ab trko manane ke liye maine pehli baar full stack project banaya maanja😂😂😂</p>
+      <p>ab yrr aise kregi kya tu bhai se naraj hogi naah naah mujhse naraj mt ho chomu ab trko manane ke liye maine pehli baar fullstack project banaya maanja bandariya😂😂😂</p>
 
       <p>Please maaf kar de yrr sorry accept pr click kar aur mujhe jo gaali likhni hai woh likhkar bhejde mujhe lekin jyada gandi gaali mat likhna😂😂</p>
 
